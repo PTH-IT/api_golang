595fa4ca4f8548fede36944b4e5d7f8b51d4aacf
PhamThanhHau <haupham404@gmail.com>
2023-01-03 23:32:52 +0700

    update

---
+++ adapter/firebaseDB/firebasedb.go
@@ -0,0 +1,64 @@
+package firebasedb
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	firebase "firebase.google.com/go/v4"
+	"github.com/labstack/echo"
+	"google.golang.org/api/option"
+)
+
+func getfirebase(c echo.Context) error {
+	ctx := context.Background()
+	conf := &firebase.Config{
+		DatabaseURL: "https://pth-it-default-rtdb.asia-southeast1.firebasedatabase.app",
+	}
+	opt := option.WithCredentialsFile("../../../pth-it-firebase-adminsdk-i11h0-ba3394f404.json")
+	app, err := firebase.NewApp(ctx, conf, opt)
+	if err != nil {
+		return c.String(http.StatusOK, err.Error())
+	}
+	client, err := app.Database(ctx)
+	if err != nil {
+		log.Fatalln("Error initializing database client:", err)
+	}
+
+	// The app only has access as defined in the Security Rules
+	ref := client.NewRef("user_scores")
+	var get []map[string]interface{}
+	if err := ref.Get(context.TODO(), &get); err != nil {
+		log.Fatal(err)
+	}
+
+	jsonbody, _ := json.Marshal(get)
+	return c.String(http.StatusOK, string(jsonbody))
+}
+func putfirebase(c echo.Context) error {
+	ctx := context.Background()
+	conf := &firebase.Config{
+		DatabaseURL: "https://pth-it-default-rtdb.asia-southeast1.firebasedatabase.app",
+	}
+	opt := option.WithCredentialsFile("../../../pth-it-firebase-adminsdk-i11h0-ba3394f404.json")
+	app, err := firebase.NewApp(ctx, conf, opt)
+	if err != nil {
+		return c.String(http.StatusOK, err.Error())
+	}
+	client, err := app.Database(ctx)
+	if err != nil {
+		log.Fatalln("Error initializing database client:", err)
+	}
+
+	// The app only has access as defined in the Security Rules
+	ref := client.NewRef("user_scores/" + fmt.Sprint(1))
+
+	if err := ref.Set(context.TODO(), map[string]interface{}{"score": 40}); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("score added/updated successfully!")
+	return c.String(http.StatusOK, "hello")
+}
---
+++ adapter/monggoDB/monggodb.go
@@ -0,0 +1,73 @@
+package monggodb
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func getmongo(c echo.Context) error {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	coll := client.Database("TEST").Collection("movies")
+	var result []bson.M
+
+	s, err := coll.Find(context.TODO(), bson.D{})
+	s.All(context.TODO(), &result)
+	if err != nil {
+		panic(err)
+	}
+	jsonData, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", jsonData)
+	return c.String(http.StatusOK, string(jsonData))
+}
+func putmongo(c echo.Context) error {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	coll := client.Database("TEST").Collection("movies")
+	title := "Back to the Future"
+
+	coll.InsertOne(context.TODO(), bson.D{{"title", title}})
+
+	return c.String(http.StatusOK, "susscess")
+}
